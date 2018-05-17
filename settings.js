032aaf042c6ff0b5108c67642a2294ffbe6fa66e
raymond <rsvvuuren@hotmail.com>
2018-05-17 23:39:35 +0200

    xx

--- settings.js
+++ settings.js
@@ -7,11 +7,10 @@ var reactGpdrSettings = {
   url: '/cookies',
   url_title: 'privacy- en cookieverklaring.',
   button: 'Accepteren',
-  level1: '1 Cookies zonder video&#39;s en zonder aanbiedingen. Deze zijn nodig om onze website te kunnen bezoeken en\n' +
+  level1: '<h4>Minimaal:</h4> Cookies die nodig zijn om onze website te kunnen bezoeken en\n' +
   '                in te kunnen loggen. Je bezoek en gegevens worden niet bijgehouden.',
-  level2: '2 Cookies met video&#39;s maar zonder aanbiedingen. Met deze cookies kun je de website bezoeken,\n' +
+  level2: '<h4>Medium:</h4> Cookies met video&#39;s maar zonder aanbiedingen. Met deze cookies kun je de website bezoeken,\n' +
   '                inloggen en video&#39;s bekijken. Je bezoek en gegevens worden bijgehouden.',
-  level3: '3 Cookies met video&#39;s en aanbiedingen. Met deze cookies werkt de website optimaal. Je bezoek wordt\n' +
+  level3: '<h4>Optimaal:</h4> Cookies met video&#39;s, aanbiedingen en socialnetworks. Met deze cookies werkt de website optimaal. Je bezoek wordt\n' +
   '                bijgehouden zodat we onze website kunnen verbeteren en je aanbiedingen kunnen doen.',
-  iFrameBlob: '<html><h3>You are not allowed to view this resource, change your <a href="' + window.location.href.replace(window.location.hash, '') + '#cookieConsent" target="_top">cookiesettings</a></h3></html>'
 };
